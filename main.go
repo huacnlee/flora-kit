cb6ce2c4db1107407a8fc003ffb4b0919232a36d
zhaojames0707 <zhaojames0707@hotmail.com>
2018-08-05 10:22:41 +0800

    fix bug of unparsed flags

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	var configFile, geoipdb string
 	flag.StringVar(&configFile, "s", "flora.default.conf", "specify surge config file")
 	flag.StringVar(&geoipdb, "d", "geoip.mmdb", "specify geoip db file")
+	flag.Parse()
 	flora.Run(configFile, geoipdb)
 
 }
